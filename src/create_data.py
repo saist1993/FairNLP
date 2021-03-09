20ccd911ffe13c73658e8b1ad8889fd2dc8159ba
gmaheshwari <gaurav.maheshwari@inria.fr>
2021-03-09 19:41:06 +0100

    slight debug

--- src/create_data.py
+++ src/create_data.py
@@ -332,7 +332,7 @@ class BiasinBiosSimple(WikiSimpleClassification):
         # Find all professional. Create a professional to id list
         all_profession = list(set([t['p'] for t in train]))
         professional_to_id = {profession:index for index, profession in enumerate(all_profession)}
-        pickle.dump(professional_to_id, open(self.data_dir + Path('profession_to_id.pickle', "wb")))
+        pickle.dump(professional_to_id, open(self.data_dir + Path('profession_to_id.pickle'), "wb"))
 
         # Tokenization and id'fying the profession
         train_processed = self.transform_dataframe_to_dict(data=train, tokenizer=self.tokenizer)
