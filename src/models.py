05c9e3b7fb2817039c83a0df147065a02d74af07
gmaheshwari <gaurav.maheshwari@inria.fr>
2021-03-15 18:08:13 +0100

    back to basic but without dropout

--- src/models.py
+++ src/models.py
@@ -146,16 +146,16 @@ class BiLSTMAdv(nn.Module):
 
         if self.noise_layer:
             m = torch.distributions.laplace.Laplace(torch.tensor([0.0]), torch.tensor([laplace(self.eps, 1)]))
-            # max_hidden = torch.max(hidden, 1, keepdims=True)[0]
-            # min_hidden = torch.min(hidden, 1, keepdims=True)[0]
-            # hidden = (hidden - min_hidden)/ (max_hidden - min_hidden)
-            hidden = hidden/torch.norm(hidden, keepdim=True)
+            max_hidden = torch.max(hidden, 1, keepdims=True)[0]
+            min_hidden = torch.min(hidden, 1, keepdims=True)[0]
+            hidden = (hidden - min_hidden)/ (max_hidden - min_hidden)
+            # hidden = hidden/torch.norm(hidden, keepdim=True)
             hidden = hidden + m.sample(hidden.shape).squeeze().to(self.device)
 
 
         # hidden = hidden/torch.norm(hidden, keepdim=True)
 
-        prediction = self.fc(self.dropout(hidden))
+        prediction = self.fc(hidden)
         adv_output = self.adv(GradReverse.apply(hidden))
 
         # prediction = [batch size, output dim]
