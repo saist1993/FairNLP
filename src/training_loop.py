0b87a91c759bb1fa771ad9ec00285da3d4938ce3
gmaheshwari <gaurav.maheshwari@inria.fr>
2021-04-01 10:50:17 +0200

    intirm commit

--- src/training_loop.py
+++ src/training_loop.py
@@ -283,9 +283,9 @@ def train_adv_three_phase_custom(model, iterator, optimizer, criterion, device, 
                 Train Freeze (Embedder) + Classifier
                 Train Freeze (Embedder) + Adv
             """
-
-            if phase == 'recover':
-                model.freeze_unfreeze_embedder(freeze=True)
+            #
+            # if phase == 'recover':
+            #     model.freeze_unfreeze_embedder(freeze=True)
 
             # --- train Embedder and Classifier
             model.freeze_unfreeze_adv(freeze=True)
