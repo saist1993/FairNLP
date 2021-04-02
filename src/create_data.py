9833684231bc16ced98769581d97558cc92266be
gmaheshwari <gaurav.maheshwari@inria.fr>
2021-04-02 09:28:37 +0200

    larger final data

--- src/create_data.py
+++ src/create_data.py
@@ -463,9 +463,9 @@ class BiasinBiosSimpleAdv(WikiSimpleClassification):
         assert self.is_regression == False
 
 
-        train = self.read_data("../data/bias_in_bios/train.pickle")[:1500]
-        dev = self.read_data("../data/bias_in_bios/dev.pickle")[:100]
-        test = self.read_data("../data/bias_in_bios/test.pickle")[:100]
+        train = self.read_data("../data/bias_in_bios/train.pickle")
+        dev = self.read_data("../data/bias_in_bios/dev.pickle")
+        test = self.read_data("../data/bias_in_bios/test.pickle")
 
         # Find all professional. Create a professional to id list
         try:
