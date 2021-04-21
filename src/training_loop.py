810f966a1739f4b654d7919c21566c8af24cfd7d
gmaheshwari <gaurav.maheshwari@inria.fr>
2021-04-21 12:32:21 +0200

    debuged training loop.

--- src/training_loop.py
+++ src/training_loop.py
@@ -924,7 +924,6 @@ def three_phase_training_loop(
                     model.classifier.apply(initialize_parameters)
                     reset_classifier = False
 
-        phase = 'perturbate'
         print(f"current phase: {phase}")
 
         start_time = time.monotonic()
