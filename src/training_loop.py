96b07d4a8387db397cddca46c003895c1f115605
gmaheshwari <gaurav.maheshwari@inria.fr>
2021-05-03 14:52:46 +0200

    increasing lr now

--- src/training_loop.py
+++ src/training_loop.py
@@ -368,7 +368,7 @@ def train_adv_three_phase_custom(model, iterator, optimizer, criterion, device, 
             model.freeze_unfreeze_classifier(freeze=False)
 
             if phase == 'recover':
-                    unfreeze(optimizer, model=model, layer='classifier', lr=0.007)
+                    unfreeze(optimizer, model=model, layer='classifier', lr=0.005)
 
             # freeze(optimizer, model=model, layer='adversary')
             model.freeze_unfreeze_adv(freeze=True)
